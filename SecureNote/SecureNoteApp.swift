88d3899dc2e23589b5ff6bcc8eb2036cf91a7fe1
dogukanogan <dogukanogan35@gmail.com>
2025-11-16 18:56:51 +0300

    Initial Commit

---
+++ SecureNote/SecureNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureNoteApp.swift
+//  SecureNote
+//
+//  Created by Doğukan Ogan on 16.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SecureNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecureNoteTests/SecureNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecureNoteTests.swift
+//  SecureNoteTests
+//
+//  Created by Doğukan Ogan on 16.11.2025.
+//
+
+import Testing
+@testable import SecureNote
+
+struct SecureNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
